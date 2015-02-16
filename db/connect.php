75dc9cf1c6b0d10250358fd765893c64ad34fc54
XBow36 <XBow36@users.noreply.github.com>
2015-02-16 01:00:03 -0700

    removed my password

--- db/connect.php
+++ db/connect.php
@@ -1,6 +1,6 @@
 <?php
-
-$db = new mysqli("oniddb.cws.oregonstate.edu", "liux5-db", "tJ2GhtEQK8tz8Dip", "liux5-db");
+//took out the password, since the files on the OSU server will still work and is hidden from public
+$db = new mysqli("oniddb.cws.oregonstate.edu", "liux5-db", "xxxxxxxxxxxxxx", "liux5-db");
 if($db->connect_errno) {
 //echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " .$db->connect_error; 
 }
